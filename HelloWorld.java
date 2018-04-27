a828c9d26ac12244d0f7479e295e26e6b3612447
Édouard LUMET <edouard@hello-community.fr>
2018-04-27 22:11:32 +0200

    Hello World !

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/** Classe implémentant un Hello Word
+ * en JAVA.
+ *
+ */
+public class HelloWorld {
+
+  /** Programme principal.
+   *
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World !");
+  }
+
+}
