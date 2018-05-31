538f8683f26491e090d56e6338495fd0c2a285dc
cAribaud <christian@aribaud.net>
2018-05-31 11:22:47 +0200

    Ajout menubar et fonction exit

--- src/controleur/Launcher.java
+++ src/controleur/Launcher.java
@@ -1,6 +1,9 @@
 package controleur;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Launcher {
 	
@@ -12,8 +15,25 @@ public class Launcher {
 		
 		JFrame application = new JFrame(APPLICATION_NAME);
 		application.setSize(default_width, default_height);
-		
+		application.setJMenuBar(buildApplicationMenuBar(application));
 		application.setVisible(true);
 	}
 
+	private static JMenuBar buildApplicationMenuBar(JFrame f) {
+		JMenu file = new JMenu("File");
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		
+		exit.addActionListener(e -> {
+			f.dispose();
+		});
+		
+		file.add(exit);
+		
+		JMenuBar appBar = new JMenuBar();
+		
+		appBar.add(file);
+		
+		return appBar;
+	}
 }
