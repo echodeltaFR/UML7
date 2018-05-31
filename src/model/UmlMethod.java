19c12eb67ee123bd8e2ee0c67a3d56591413e124
cAribaud <christian@aribaud.net>
2018-05-31 11:31:10 +0200

    Correction UmlMethod

--- src/model/UmlMethod.java
+++ src/model/UmlMethod.java
@@ -1,22 +1,17 @@
-package Agile;
+package model;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UmlMethod {
 
 	private ArrayList<String> params;
-	private String returnValue;
+	private String returnType;
 	private String name;
-	private String visibility;
-	private String modifier;
 	
-	public UmlMethod(String visibility, 
-			String modifier, 
-			ArrayList<String> params,  
+	public UmlMethod(ArrayList<String> params,  
 			String returnValue,
 			String name) {
-		this.visibility = visibility;
-		this.modifier = modifier;
-		this.returnValue = returnValue;
+		this.returnType = returnValue;
 		this.params = params;
 		this.name = name;
 	}
@@ -25,20 +20,32 @@ public class UmlMethod {
 		this.params.addAll(params);
 	}
 	
-	public void addParams(String params) {
-		this.params.add(params);
+	public void addParams(String param) {
+		this.params.add(param);
 	}
 	
-	public void setReturn(String returnValue) {
-		this.returnValue = returnValue;
+	public void setReturn(String returnType) {
+		this.returnType = returnType;
 	}
 	
-	public void removeReturn(String returnValue) {
-		this.returnValue = null;
+	public void removeReturn() {
+		this.returnType = null;
+	}
+	
+	public String getReturnType() {
+		return this.returnType;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public List<String> getParams(){
+		return this.params;
 	}
 	
 	public void removeParams(ArrayList<String> params) {
-		this.params.remove(params);
+		this.params.removeAll(params);
 	}
 	
 }
