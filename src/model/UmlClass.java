8789f59b6e349d13142a00d71677bf319e3574c4
minghao217 <wmhaijsn@gmail.com>
2018-05-31 15:52:13 +0200

    Revert "Test vue classe"
    This reverts commit f473599a665be8c58114a048e589b68c6a51b3a0.

--- src/model/UmlClass.java
+++ src/model/UmlClass.java
@@ -5,11 +5,16 @@ public class UmlClass {
 	 *le Nom de class
 	 */
 	private String className;
+//	private ArrayList<UmlMethod> umlMethod;
+//	private ArrayList<UmlAttribute> umlAttribute;
+//	private ArrayList<Modifier> modifier;
 	/**
 	 * le stractu
 	 * @param name le Nom de class
 	 */
 	public UmlClass(String name) {
+//		umlMethod = new ArrayList<UmlMethod>();
+//		umlAttribute = new ArrayList<UmlAttribute>;
 		className = name;
 	}
 	public void setName(String name) {
@@ -19,4 +24,15 @@ public class UmlClass {
 	public String getName() {
 		return className;
 	}
+//	public 	void addMethod(Umlmethod method) {
+//		umlMethod.add(method);
+//	}
+//	public 	void addAttribute(UmlAttribute attribute) {
+//		umlAttribute.add(attribute);
+//	}
+//	
+//	public addModifier(Modifier m) {
+//		modifier.add(m);
+//	}
+	
 }
