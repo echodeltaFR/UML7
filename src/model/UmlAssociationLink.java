e7e6a1b46063c4169609717df82c505e85466c21
Charly Courilleau <contact@charly.computer>
2018-05-31 11:50:21 +0200

    Ajout des relations : aggregation, association, héritage.

---
+++ src/model/UmlAssociationLink.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Association Link
+ * @author Charly Courilleau
+ *
+ */
+public class UmlAssociationLink extends UmlRelationShip {
+
+	public UmlAssociationLink(UmlClass theClassA,  UmlClass theClassB) {
+		super(theClassA, theClassB);
+	}
+	
+}
---
+++ src/model/UmlRelationShip.java
@@ -0,0 +1,62 @@
+package model;
+
+/**
+ * Uml Relationship
+ * @author Charly Courilleau
+ */
+public abstract class UmlRelationShip {
+	
+	/** Class A **/
+	private UmlClass classA;
+	
+	/** Class B **/
+	private UmlClass classB;
+	
+	/** classA's role **/
+	private String roleA;
+	
+	/** classB's role **/
+	private String roleB;
+	
+	/** Relation name **/
+	private String relationName;
+
+
+	protected UmlRelationShip(UmlClass elementA, UmlClass elementB) {
+		this.classA = elementA;
+		this.classB = elementB;
+	}
+	
+	public String getRoleA() {
+		return roleA;
+	}
+
+	public void setRoleA(String roleA) {
+		this.roleA = roleA;
+	}
+
+	public String getRoleB() {
+		return roleB;
+	}
+
+	public void setRoleB(String roleB) {
+		this.roleB = roleB;
+	}
+
+	public UmlClass getClassA() {
+		return classA;
+	}
+
+	public UmlClass getClassB() {
+		return classB;
+	}
+
+	public String getRelationName() {
+		return relationName;
+	}
+
+	public void setRelationName(String relationName) {
+		this.relationName = relationName;
+	}
+
+}
