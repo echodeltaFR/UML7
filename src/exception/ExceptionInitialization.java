879bc567fc58e1b67226c506e7b859e3106ad144
minghao217 <wmhaijsn@gmail.com>
2018-06-14 09:20:48 +0200

    supprimer ExceptionInitialization.java

--- src/exception/ExceptionInitialization.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ExceptionInitialization extends ExceptionUml{
-	public ExceptionInitialization(String message) {
-		super("initialization failed : " + message);
-	}
-}
